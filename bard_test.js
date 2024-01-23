5e640eeb07b82455d9a77b0b2212fcd2bb8d2183
ricky0601 <ricky2425@naver.com>
2024-01-23 12:22:52 +0900

    bardCheck

--- bard_test.js
+++ bard_test.js
@@ -5,11 +5,11 @@ async function testAssistant() {
     const assistant = new BardAPI();
 
     // Set session information for authentication
-    await assistant.setSession('__Secure-3PSID', 'fgis1YNKiWr4W6KdcWtDa6Psl9Wi4wVNaeZm8FDjL1SWSymbSdH7hlU2280TI7gZbYhc7w.'); // or '__Secure-3PSID'
+    await assistant.setSession('__Secure-3PSID', 'fgis1XCBtuArbE_nrxFaeyOzky3sS0Bk7tYC_4476VdxbqeixGpPc1hLTz_IRwRZ63OSSw.'); // or '__Secure-3PSID'
     // ...
-
+    const url = "쿠팡";
     // Send a query to Bard
-    let question = "오늘 날씨는 어떄?";
+    let question = url + " 사이트 주소를 알려줘";
     const response = await assistant.getBardResponse(question);
     console.log('Bard:', response.content);
     alert('Bard:', response.content);
