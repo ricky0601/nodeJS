9c38d3a1a941fa7b0125f0c09f929b264b5bcfdf
ricky0601 <ricky2425@naver.com>
2024-02-14 12:29:40 +0900

    gemini_test 0214

--- gemini_test/index.js
+++ gemini_test/index.js
@@ -1,15 +1,31 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
+const fs = require("fs");
 
 // Access your API key as an environment variable (see "Set up your API key" above)
-const genAI = new GoogleGenerativeAI(process.env.AIzaSyBhWxSAM1zuKSq9FNcMoc9vDCRXCix2QCE);
+const genAI = new GoogleGenerativeAI('AIzaSyBhWxSAM1zuKSq9FNcMoc9vDCRXCix2QCE');
+
+// Converts local file information to a GoogleGenerativeAI.Part object.
+function fileToGenerativePart(path, mimeType) {
+  return {
+    inlineData: {
+      data: Buffer.from(fs.readFileSync(path)).toString("base64"),
+      mimeType
+    },
+  };
+}
 
 async function run() {
-  // For text-only input, use the gemini-pro model
-  const model = genAI.getGenerativeModel({ model: "gemini-pro"});
+  // For text-and-image input (multimodal), use the gemini-pro-vision model
+  const model = genAI.getGenerativeModel({ model: "gemini-pro-vision" });
 
-  const prompt = "Write a story about a magic backpack."
+  const prompt = "이 사진을 시각장애인들도 알 수 있게 alt 속성을 작성해줘";
 
-  const result = await model.generateContent(prompt);
+  const imageParts = [
+    //fileToGenerativePart("image1.png", "image/png"),
+    fileToGenerativePart("image2.png", "image/png"),
+  ];
+
+  const result = await model.generateContent([prompt, ...imageParts]);
   const response = await result.response;
   const text = response.text();
   console.log(text);
