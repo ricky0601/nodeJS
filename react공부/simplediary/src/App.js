983df8564e7f29e536bfc3bc68c562681444ae77
ricky0601 <ricky2425@naver.com>
2024-03-03 18:58:46 +0900

    리스트 렌더링(조회)

--- react공부/simplediary/src/App.js
+++ react공부/simplediary/src/App.js
@@ -1,10 +1,36 @@
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
+import DiaryList from "./DiaryList";
+
+const dummyList = [
+  {
+    id:1,
+    author:"한동건",
+    content:"하이 1",
+    emotion: 1,
+    created_date: new Date().getTime(),
+  },
+  {
+    id:2,
+    author:"홍길동",
+    content:"하이 2",
+    emotion: 3,
+    created_date: new Date().getTime(),
+  },
+  {
+    id:3,
+    author:"아무개",
+    content:"하이 3",
+    emotion: 5,
+    created_date: new Date().getTime(),
+  },
+]
 
 function App(){
   return (
     <div className="App">
       <DiaryEditor/>
+      <DiaryList diaryList={dummyList}/>
     </div>
   );
 }
