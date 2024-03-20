833a0cee6d740a7e67e555bf113396f62011468a
ricky0601 <ricky2425@naver.com>
2024-03-20 16:24:31 +0900

    Diary Delete

--- react공부/simplediary/src/DiaryItem.js
+++ react공부/simplediary/src/DiaryItem.js
@@ -1,4 +1,4 @@
-const DiaryItem = ({author, content, created_date, emotion, id}) => {
+const DiaryItem = ({onDelete, author, content, created_date, emotion, id}) => {
     return <div className="DiaryItem">
         <div className="info">
             <span>작성자 : {author} | 감정점수 : {emotion}</span>
@@ -8,6 +8,16 @@ const DiaryItem = ({author, content, created_date, emotion, id}) => {
         <div className="content">
             {content}
         </div>
+        <button
+            onClick={()=>{
+                console.log(id);
+                if(window.confirm(`${id}번째 일기를 정말 삭제하시겠습니까?`)){
+                    onDelete(id);
+                }
+            }}
+        >
+            삭제하기
+        </button>
     </div>;
 };
 
